d9f6e630c81e667bf327b9530f62fff7572ddc1d
Kevin Amorim <kevinamorim4@gmail.com>
2020-05-30 19:28:21 +0100

    Small improvements.

---
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -0,0 +1,13 @@
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { ShoppingListComponent } from './shopping-list.component';
+
+const routes: Routes = [  { path: '', component: ShoppingListComponent } ];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class ShoppingListRoutingModule {
+
+}
